4e7ff692509efe684c7df54d818255e32a025ba7
ARAVINDH M <aravindhsivaji007@gmail.com>
2025-07-26 22:47:57 +0530

    Added API

--- server/index.js
+++ server/index.js
@@ -7,8 +7,19 @@ import UserRoutes from "./routes/User.js";
 dotenv.config();
 
 const app = express();
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://fittrack-ar.netlify.app"
+];
+
 app.use(cors({
-  origin: "http://localhost:3000" , // your React frontend
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
 }));
 app.use(express.json({ limit: "50mb" }));
